d831347ac7727a88f83e5b414e163ffea9059a37
KrasimirRaikov <raikov.krasimir@gmail.com>
2016-03-07 09:43:13 +0200

    first task database operations

---
+++ src/com/clouway/jdbc/ConnectionManager.java
@@ -0,0 +1,20 @@
+package com.clouway.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author Krasimir Raikov(raikov.krasimir@gmail.com)
+ */
+public class ConnectionManager {
+
+    public Connection getConnection(String database, String username, String password) {
+        try {
+            return DriverManager.getConnection("jdbc:postgresql://localhost/" + database, username, password);
+        } catch (SQLException e) {
+            throw new ExecutionException("can't connect");
+        }
+    }
+
+}
